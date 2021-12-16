e88f327faafcbfdf8a36b9586a4b5dda9a26f0dc
kaushid <dinesh.k.kaushish@dematic.com>
2021-12-16 15:10:25 +0530

    Initial commit

---
+++ src/main/java/com/vanamadhuryam/calenderapp/CalenderappApplication.java
@@ -0,0 +1,12 @@
+package com.vanamadhuryam.calenderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderappApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(CalenderappApplication.class, args);
+   }
+}
---
+++ src/test/java/com/vanamadhuryam/calenderapp/CalenderappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.vanamadhuryam.calenderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderappApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+}
